4f16ddd8789354cb705b185a9150f90923507e86
Tanveer Ahmed <tanveeransari0947@gmail.com>
2026-01-09 00:05:54 +0530

    complete footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
+import Footer from './components/Footer';
+import AllRooms from './pages/AllRooms';
 
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
-    <div>
+<div className="min-h-screen flex flex-col">  
      {!isOwnerPath && <Navbar/> } 
-     <div>
+     <div className='flex-1'>
         <Routes>
              <Route path='/' element= {<Home/>} />             
+             <Route path='/rooms' element= {<AllRooms/>} />             
         </Routes>
      </div>
+        <Footer/>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
+
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
     <FeaturedDestination/>
     <ExclusiveOffers/>
     <Testimonial/>
+    <NewsLetter/>
+   
     </>
   )
 }
